cf787be000cad3174ac0845768fd7c4cccba3d3d
Martino Salvetti <tldr@inbitcoin.it>
2016-12-24 14:44:51 +0100

    Add int->perm

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -14,17 +14,42 @@ def permutation_to_variable_positions(perm):
 
 
 def permutation_to_integer(perm):
-    """Permutations (0÷n-1) to integer"""
+    """Permutation (0÷n-1) to integer"""
     positions = permutation_to_variable_positions(perm)
-    h = 1
-    w = 1
+    base = 1
+    weight = 1
     integer = 0
     for p in positions[::-1]:
-        integer += p * w
-        h += 1
-        w *= h
+        integer += p * weight
+        base += 1
+        weight *= base
     return integer
 
 
-def integer_to_permutation(integer):
-    pass
+def integer_to_variable_positions(integer, n):
+    """Permutation (0÷n-1) to variable positions"""
+    base = 2
+    positions = []
+    for _ in range(n-1):
+        positions.append(integer % base)
+        integer //= base
+        base += 1
+    return reversed(positions)
+
+
+def variable_to_absolute_positions(var):
+    """Variable positions to absolute positions"""
+    absolute = [None] * len(var)
+    pointers = list(range(len(var)))
+    for e, p in enumerate(var):
+        absolute[pointers[p]] = e
+        del pointers[p]
+
+    return absolute
+
+
+def integer_to_permutation(integer, n):
+    """Integer (0÷n-1) to permutation"""
+    var_pos = list(integer_to_variable_positions(integer, n))
+    var_pos.append(0)
+    return variable_to_absolute_positions(var_pos)
--- permutations/tests.py
+++ permutations/tests.py
@@ -9,7 +9,8 @@ class Tests(TestCase):
         """Test perm_to_positions"""
         self.assertEqual(permutation_to_variable_positions([0]), [])
         self.assertEqual(permutation_to_variable_positions([0, 1]), [0])
-        self.assertEqual(permutation_to_variable_positions([3, 2, 1, 0]), [3, 2, 1])
+        self.assertEqual(permutation_to_variable_positions(
+            [3, 2, 1, 0]), [3, 2, 1])
         self.assertEqual(permutation_to_variable_positions(
             [2, 0, 4, 1, 3]), [1, 2, 0, 1])
 
@@ -19,11 +20,24 @@ class Tests(TestCase):
         self.assertEqual(permutation_to_integer([0, 1, 2, 3, 4, 5]), 0)
         self.assertEqual(permutation_to_integer([2, 0, 4, 1, 3]), 37)
 
+    def test_integer_to_variable_positions(self):
+        """Test integer_to_variable_positions"""
+        f = lambda *args: list(integer_to_variable_positions(*args))
+        self.assertEqual(f(0, 5), [0, 0, 0, 0])
+        self.assertEqual(f(119, 5),[4, 3, 2, 1])
+        self.assertEqual(f(37, 5), [1, 2, 0, 1])
+
+    def test_variable_to_absolute_positions(self):
+        """Test variable_to_absolute_positions"""
+        f = variable_to_absolute_positions
+        self.assertEqual(f([0, 0, 0, 0, 0]), [0, 1, 2, 3, 4])
+        self.assertEqual(f([1, 2, 0, 1, 0]), [2, 0, 4, 1, 3])
+
     def test_integer_to_permutation(self):
         """Test perm_to_int"""
-        # self.assertEqual(integer_to_permutation(0), [])
-        # self.assertEqual(integer_to_permutation([0, 1, 2, 3, 4, 5]), 0)
-        # self.assertEqual(integer_to_permutation([2, 0, 4, 1, 3]), 37)
+        self.assertEqual(integer_to_permutation(0, 5), [0, 1, 2, 3, 4])
+        self.assertEqual(integer_to_permutation(119, 5), [4, 3, 2, 1, 0])
+        self.assertEqual(integer_to_permutation(37, 5), [2, 0, 4, 1, 3])
 
 
 if __name__ == "__main__":
