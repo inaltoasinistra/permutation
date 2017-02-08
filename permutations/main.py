ee0868c7f63feb3b23a143815c42ddea9ffec54d
Martino Salvetti <tldr@inbitcoin.it>
2017-02-09 00:05:20 +0100

    First implementation of argparse

--- permutations/main.py
+++ permutations/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
-"""
+# -*- coding: utf-8 -*-
+u"""
 usage:
     main.py encode
     main.py decode
@@ -12,24 +12,38 @@ e.g. tragedia malinteso attorno lacuna invece michele produrre vispo
      4♣ 8♦ 7♦ 2♣ K♠ Q♠ 7♠ J♦ 9♠ 9♣ A♣ Q♦ 5♦ 10♠ 3♠ K♦ K♣ 10♦ 10♣ 2♠
      9♦ J♠ 8♣ A♠ Q♣ 6♠ J♣ 6♦ 6♣ 7♣ 5♣
 """
-from getpass import getpass
 import sys
-
+import argparse
 from ordering import encode, decode
 from mnemonic import mnemonic_to_integer, integer_to_mnemonic
 
+
 def main():
-    if not sys.argv[1:]:
-        print(__doc__)
-        sys.exit()
-    if sys.argv[1] == 'encode':
-        seed = getpass('BIP39 seed: ')
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('mode', type=str,
+                        help='encode or decode')
+    # parser.add_argument('integers', metavar='N', type=int, nargs='+',
+    #                     help='an integer for the accumulator')
+    parser.add_argument('--ordering', dest='ordering', type=str,
+                        default='French',
+                        help='objects of the permutation')
+    parser.add_argument('--labels', dest='labels', type=str,
+                        default='Symbols',
+                        help='~ of objects')
+    parser.add_argument('--language', dest='language', type=str,
+                        default='english',
+                        help='~ of the seed wordlist')
+
+    args = parser.parse_args()
+
+    if args.mode == 'encode':
+        seed = input('BIP39 seed: ')
         integer = mnemonic_to_integer(seed.split())
-        print(' '.join(decode('French', 'Symbols', integer)))
-    elif sys.argv[1] == 'decode':
-        perm = getpass('Cards permutation: ')
+        print(' '.join(decode(args.ordering, args.labels, integer)))
+    elif args.mode == 'decode':
+        perm = input('Cards permutation: ')
         integer = encode(perm.split())
-        print(' '.join(integer_to_mnemonic(integer, 'english')))
+        print(' '.join(integer_to_mnemonic(integer, args.language)))
     else:
         print(__doc__)
         sys.exit()
